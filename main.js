03b01de8402836bb6b2b563bbb041b666f0b024b
monikalipsa <mgp9483@gmail.com>
2024-02-03 11:46:30 -0300

    function validar

--- main.js
+++ main.js
@@ -1,57 +1,41 @@
-// console.log("Hola mundo");
 
-
-// declaracion de variables:
-                  //  let bodyElement = document.body;
-                  //  console.log (bodyElement);
-
-//si quiero ver el bodi en consola lo puedo hacer de 2 formas
-           //1) console.log(document.body)
-           //2) con variable:
-                          // let bodyElement =document.body;
-                          //console.log (bodyElement)
-
- //----------PARA SELECCIONAR POR ID-------------
-         //  let elementoId = document.getElementById('titulo');
-         //  console.log (elementoId);     
-
-
-  //----------PARA SELECCIONAR POR CLASE-------------              
-//   let elementoClass = document.getElementsByClassName('header');
-//    console.log (elementoClass);     
-
-
-  //----------PARA SELECCIONAR POR TAG-------------   
-
-//   let elementoTag = document.getElementsByTagName('h2');
-//   console.log (elementoTag);     
-
-    //----------PARA CREAR UN ELEMENTO-------------  
-
-   //  let nuevoParrafo = document.createElement('p');
-   //  nuevoParrafo.innerHTML = "Parrafo creado";
-
-   //  bodyElement.appendChild(nuevoParrafo)
 
    // ----------------------------------FORMULARIO-----------------------------------------
 
    function registrarse (){
          const nombre = document.getElementById ("nombre") .value;
          const apellido = document.getElementById ("apellido") .value;
+         const telefono = document.getElementById ("telefono") .value;
          const email = document.getElementById ("email") .value;
+         const tipoContacto = document.getElementById ("tipoContacto") .value;
+         const asunto = document.getElementById ("asunto") .value;
+         const descripcion = document.getElementById ("descripcion") .value;
          const password = document.getElementById ("password") .value;
+         const confirmPassword = document.getElementById ("confirmPassword") .value;
+
+      //  __________________________________________________________
 
 
-         if (nombre == '') {
-            document.getElementById ('error_nombre').innerHTML = 'El campo es obligatorio';
-         } else {document.getElementById ('error_nombre').innerHTML = '';              
+          if (nombre == '') {
+             document.getElementById ('error_nombre').innerHTML = 'El campo es obligatorio';
+          } else {document.getElementById ('error_nombre').innerHTML = '';
           }
             
-          console.log (nombre,apellido,email,password)
-
+           console.log (nombre,apellido,telefono,email, tipoContacto, asunto, descripcion, password, confirmPassword);
 
           } 
-            
 
 
-      // console.log (nombre)
+     function validar(elemen){
+      const value = elemen.value;
+      const name = elemen.name;
+      if (value === ' '){
+        document.getElementById('error_' + name).innerHTML = 'El campo es obligatorio'
+      } else {
+        document.getElementById ('error_' + name).innerHTML = '';
+      }
+     }
+
+     
+
+      
