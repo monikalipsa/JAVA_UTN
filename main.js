37fbbd0b42f1ebd33ae6ea897ace184ab88e5938
monikalipsa <mgp9483@gmail.com>
2024-02-04 18:55:52 -0300

    config mensaje txt

--- main.js
+++ main.js
@@ -1,41 +1,56 @@
 
+  //   function registrarse (){
+  //         const nombre = document.getElementById ("nombre") .value;
+  //         const apellido = document.getElementById ("apellido") .value;
+  //         const telefono = document.getElementById ("telefono") .value;
+  //         const email = document.getElementById ("email") .value;
+  //         const tipoContacto = document.getElementById ("tipoContacto") .value;
+  //         const asunto = document.getElementById ("asunto") .value;
+  //         const descripcion = document.getElementById ("descripcion") .value;
+  //         const password = document.getElementById ("password") .value;
+  //         const confirmPassword = document.getElementById ("confirmPassword") .value;
 
-   // ----------------------------------FORMULARIO-----------------------------------------
-
-   function registrarse (){
-         const nombre = document.getElementById ("nombre") .value;
-         const apellido = document.getElementById ("apellido") .value;
-         const telefono = document.getElementById ("telefono") .value;
-         const email = document.getElementById ("email") .value;
-         const tipoContacto = document.getElementById ("tipoContacto") .value;
-         const asunto = document.getElementById ("asunto") .value;
-         const descripcion = document.getElementById ("descripcion") .value;
-         const password = document.getElementById ("password") .value;
-         const confirmPassword = document.getElementById ("confirmPassword") .value;
-
-      //  __________________________________________________________
+  // //     //  __________________________________________________________
 
 
-          if (nombre == '') {
-             document.getElementById ('error_nombre').innerHTML = 'El campo es obligatorio';
-          } else {document.getElementById ('error_nombre').innerHTML = '';
-          }
+  //          if (nombre == '') {
+  //             document.getElementById ('error_nombre').innerHTML = 'El campo es obligatorio';
+  //          } else {document.getElementById ('error_nombre').innerHTML = '';
+  //          }
             
-           console.log (nombre,apellido,telefono,email, tipoContacto, asunto, descripcion, password, confirmPassword);
+  //           console.log (nombre,apellido,telefono,email, tipoContacto, asunto, descripcion, password, confirmPassword);
 
-          } 
+  //          } 
 
 
-     function validar(elemen){
-      const value = elemen.value;
-      const name = elemen.name;
-      if (value === ' '){
-        document.getElementById('error_' + name).innerHTML = 'El campo es obligatorio'
-      } else {
-        document.getElementById ('error_' + name).innerHTML = '';
+    //   function validar(elemen){
+    //    const value = elemen.value;
+    //    const name = elemen.name;
+    //    if (value === ' '){
+    //      document.getElementById('error_' + name).innerHTML = 'El campo es obligatorio'
+    //  } else {
+    //      document.getElementById ('error_' + name).innerHTML = '';
+    //    }
+    //   }
+
+    //  ----------------------------------------------------------------
+    const getMensaje = () => {
+      const http = new XMLHttpRequest();
+      http.onreadystatechange = () => {
+           console.log ("onreadystatechange");
+           console.log (http.readyState);
+           console.log (http.status);
+                 
+           if (http.readyState == 4 && http.status == 200){
+                
+                document.getElementById("mensaje").innerHTML = http.responseText;
       }
-     }
+  }; 
+  http.open('GET', 'http://localhost/BACKAV/entrega_mod2_js/gracias.txt', true)
+  http.send()
+  
+  }
+const registrarse =() => {
+  getMensaje()
 
-     
-
-      
+}
